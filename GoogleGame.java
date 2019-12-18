05a386aa4e3e2086f29d42beead48a6f74243964
drawdehe <44729388+drawdehe@users.noreply.github.com>
2019-12-18 14:46:43 +0100

    Update GoogleGame.java

--- GoogleGame.java
+++ GoogleGame.java
@@ -5,7 +5,7 @@
  * A game where two players battle to get the most Google hits.
  * 
  * Rules: Every round, the game randomly selects a letter. The players are supposed to find a word of at least
- * 3 letters that has the most hits on Google. After 11 rounds, the game ends. The person with the highest score wins.
+ * 3 letters that has the most hits on Google. After 11 rounds, the game ends. The player with the highest score wins.
  *
  */
 import java.util.Random;
